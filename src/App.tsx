7a1882855f22092c2eb5b833af6edf14cbc0f95d
Levileb <127000037+Levileb@users.noreply.github.com>
2025-03-11 16:38:53 +0800

    init-commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div className="header">
+      <div className="title">
+        <h1>Lev.</h1>
+      </div>
+      <div className="nav-bar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+     </div>
+    </>
+  )
+}
+
+
+export default App
