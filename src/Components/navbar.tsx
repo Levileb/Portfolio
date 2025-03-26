a085bdf56e191b526bc88605c18ca064e6975c27
Levileb <127000037+Levileb@users.noreply.github.com>
2025-03-26 17:01:34 +0800

    MINOR-UPDATE

---
+++ src/Components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav className="p-4 bg-gray-800 text-white">
+            
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/home.tsx
+++ src/home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Navbar from './Components/navbar';
 
 const Home: React.FC = () => {
     return (
         <div>
-            <h1>Welcome to My Portfolio</h1>
-            <p>This is the home page of my portfolio.</p>
+           <Navbar />
         </div>
     );
 };
